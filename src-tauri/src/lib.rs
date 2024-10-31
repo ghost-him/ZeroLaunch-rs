f1a88aef8f1a72308a27e94cd2d37c889ebdfd5a
ghost-him <ghost-him@outlook.com>
2024-10-31 23:52:37 +0800

    同步

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -9,6 +11,23 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+
+            window
+                .set_size(tauri::Size::Physical(tauri::PhysicalSize {
+                    width: 800,
+                    height: 600,
+                }))
+                .unwrap();
+            window.set_resizable(false).unwrap();
+
+            window.set_decorations(false).unwrap();
+
+            window.set_always_on_top(true).unwrap();
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

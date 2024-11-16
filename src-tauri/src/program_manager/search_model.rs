a354712e740089c634e36eb6af4457525ad9e0a7
surf1over <yellowstar168@outlook.com>
2024-11-16 20:44:27 +0800

    完成了初步的搜索算法

--- src-tauri/src/program_manager/search_model.rs
+++ src-tauri/src/program_manager/search_model.rs
@@ -28,10 +28,73 @@ pub type ScoreAdjusterFn = fn(f64) -> f64;
 /// user_input: 用户输入的字符串
 pub type SearchModelFn = fn(Arc<Program>, &str) -> f64;
 
-pub fn StandardSearchFn(program: Arc<Program>, user_input: &str) -> f64 {
+/// 调用函数指针利用不同公式进行权重计算
+/// operation：目标权重公式
+pub fn calculate_weight(program_name: &str, input_name: &str, operation: SearchAlgorithmFn) -> f64{
+    operation(program_name,input_name)
+}
+
+/// 调用函数调整权重得分
+/// operation: 权重调整公式
+pub fn score_adjust(score: f64, operation: ScoreAdjusterFn) -> f64{
+    operation(score)
+}
+
+/// 得分权重调整公式log2
+pub fn adjust_score_log2(origin_score : f64) -> f64{
+    3.0 * (origin_score + 1.0).log2()
+}
+
+/// 权重计算最短编辑距离
+pub fn shortest_edit_dis(compare_name: &str,input_name: &str) -> f64{
+    let input_length = input_name.len();
+    let compare_length = compare_name.len();
+    let mut dp = vec![vec![256;compare_length + 1];input_length + 1];
+    for i in 0..=input_length {
+        dp[i][0] = i;
+    }
+    for i in 0..=compare_length {
+        dp[0][i] = i;
+    }
+    for i in 1..=input_length{
+        for j in 1..=compare_length{
+            dp[i][j] = std::cmp::min(dp[i-1][j]+1, dp[i][j-1]+1);
+            if compare_name.chars().nth(i-1) == input_name.chars().nth(j-1){
+                dp[i][j] = std::cmp::min(dp[i][j], dp[i-1][j-1]);
+            }
+            else{
+                dp[i][j] = std::cmp::min(dp[i][j], dp[i-1][j-1]+1);
+            }
+        }
+    }
+    let min_operations = dp[input_length][compare_length];
+    let value : f64 = (1 - min_operations/input_length) as f64;
+    score_adjust(input_length as f64, adjust_score_log2) * f64::exp(3.0 * value - 2.0)
+}
+
+/// 权重计算KMP
+pub fn KMP(compare_name: &str,input_name: &str) -> f64{
+    let mut end_pos : f64 = 0.0;
+    for i in 0..std::cmp::min(compare_name.len(), input_name.len()){
+        if compare_name.chars().nth(i) == input_name.chars().nth(i){
+            end_pos +=1.0;
+        }else{
+            break;
+        }
+    }
+    if let Some(pos) = compare_name.find(input_name) {
+        end_pos = end_pos + pos as f64;
+    } 
+    else{}
+    end_pos
+}
+
+pub fn standard_search_fn(program: Arc<Program>, user_input: &str) -> f64 {
     // todo: 完成这个实现，如果使用到了什么子算法，用上面的模块实现出来再完成这个就可以了
     // program中的字符串与user_input都已经是预处理过了，不再需要预处理了
-    0.0
+    let mut score : f64 = calculate_weight(&program.show_name,user_input,shortest_edit_dis);
+    score = score * score_adjust((user_input.len() as f64)/(program.show_name.len() as f64), adjust_score_log2);
+    score = score + calculate_weight(&program.show_name,user_input, KMP)
 }
 
 /// 去除一个程序名中的版本号

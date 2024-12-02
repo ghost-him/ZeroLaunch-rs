1c6b0e57d9d9b20a2f10c6520cf344c0a83f1e88
ghost-him <ghost-him@outlook.com>
2024-12-02 16:10:15 +0800

    更改版本号

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -250,7 +250,7 @@ fn init_system_tray(app: &mut App) {
     let tray_icon = TrayIconBuilder::new()
         .menu(&menu)
         .icon(Image::from_path(icon_path).unwrap())
-        .tooltip("ZeroLaunch-rs v0.1.0")
+        .tooltip("ZeroLaunch-rs v0.1.1")
         .menu_on_left_click(false)
         .build(handle)
         .unwrap();
